5eb739023ed2333cd183e6263b435f9435568898
Afaq Hussain <hussainafaq18@gmail.com>
2022-09-12 05:58:30 +0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 export const Header = () => {
   return (
+    <>
+    <h1>Made by Afaq</h1>
     <h2>
         Expense Tracker
     </h2>
+    </>
   )
 }
 export default Header;
